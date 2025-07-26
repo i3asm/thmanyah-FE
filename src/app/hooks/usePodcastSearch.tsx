7a2426565b324f1aef108ec56ead48a720784214
Asim Alamri <i3asmmail@gmail.com>
2025-07-26 23:52:42 +0300

    FIX: update default BE url

--- src/app/hooks/usePodcastSearch.tsx
+++ src/app/hooks/usePodcastSearch.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {useSearchParams} from "next/navigation";
 import {ItunesSearchResponse} from "@/app/types/itunesSearchResponse";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.thmanyah.i3asm.xyz';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://thmanyahapi.i3asm.xyz';
 
 export function usePodcastSearch() {
   const [searchResult, setSearchResult] = useState<ItunesSearchResponse | null>();
